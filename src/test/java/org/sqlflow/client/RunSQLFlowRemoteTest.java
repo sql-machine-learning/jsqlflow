0276f85b79ca39b5167844b26785df327bad72c4
weiguoz <wg.z@qq.com>
2020-08-11 15:47:25 +0800

    Trace exception (#38)
    * expose exception
    
    * trace exception

--- src/test/java/org/sqlflow/client/RunSQLFlowRemoteTest.java
+++ src/test/java/org/sqlflow/client/RunSQLFlowRemoteTest.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.sqlflow.client.utils.EnvironmentSpecificSQLFlowClient;
 
 public class RunSQLFlowRemoteTest {
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     String sqlProgram = args.length == 0 ? null : args[0];
     if (StringUtils.isBlank(sqlProgram)) {
       System.out.println("skip the test due to the `sql` is empty");
@@ -22,8 +22,7 @@ public class RunSQLFlowRemoteTest {
     try {
       client.release();
     } catch (InterruptedException e) {
-      System.err.println("encounter an exception while releasing SQLFlow client");
-      throw e;
+      e.printStackTrace();
     }
   }
 }
